b80f33ef3be12b04cbafd4f2056b66aeb89ed65a
Yuri-Punzo <yuripunzo@hotmail.it>
2022-11-16 12:49:48 +0100

    global state wip

--- src/store.js
+++ src/store.js
@@ -2,5 +2,22 @@ import { reactive } from "vue";
 import axios from "axios";
 
 export const store = reactive({
-
+    //API_URL: "link to desired api"
+    error: null,
+    loading: true,
+    searchText: "",
+    callApi: (url) => {
+        axios.get(url)
+            .then(response => {
+                console.log(response);
+                /* console.log(response.data);
+                store.characters = response.data
+                store.charactersLength = response.data.length */
+                store.loading = false
+            })
+            .catch(error => {
+                console.error(error.message);
+                error = error.message
+            })
+    }
 })
