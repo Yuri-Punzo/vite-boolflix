2c0db104c2f151093253d9262ccc7fd7eda1ae69
Yuri-Punzo <yuripunzo@hotmail.it>
2022-11-16 12:37:00 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "App"
+}
+</script>
+
+<template>
+  <h1 class="text-center">Hello Motherfucker</h1>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+  margin-top: 400px;
+}
+</style>
