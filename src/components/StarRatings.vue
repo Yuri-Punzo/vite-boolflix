c443fc9a2afffdc8baffa11aad6f0c447f207abe
Yuri-Punzo <yuripunzo@hotmail.it>
2022-11-18 15:01:55 +0100

    shortened code for StarRatings system

--- src/components/StarRatings.vue
+++ src/components/StarRatings.vue
@@ -8,11 +8,7 @@ export default {
 </script>
 
 <template>
-    <div v-if="Math.ceil(itemRating / 2) === 5" class="d-flex my_rating pt-1">
-        <h5 class="pe-2">Voto:</h5>
-        <img src="../assets/img/Mario64-Star.png" v-for="star in Math.ceil(itemRating / 2)">
-    </div>
-    <div v-else-if="Math.ceil(itemRating / 2) < 5 && Math.ceil(itemRating / 2) > 0" class="d-flex my_rating pt-1">
+    <div v-if="Math.ceil(itemRating / 2) <= 5 && Math.ceil(itemRating / 2) > 0" class="d-flex my_rating pt-1">
         <h5 class="pe-2">Voto:</h5>
         <img src="../assets/img/Mario64-Star.png" v-for="star in Math.ceil(itemRating / 2)">
         <img src="../assets/img/Mario64-Star-bNw.png" v-for="starBN in (5 - (Math.ceil(itemRating / 2)))">
